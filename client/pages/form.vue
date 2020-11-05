414ae1ade1544b9bf1657016053ee8157d7aa51b
ViniFlores <viniciusflores93@gmail.com>
2020-11-05 04:13:48 -0300

    Estrutura de pastas projetos(client e api) e layout básico

---
+++ client/pages/form.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="d-flex flex-column">
+    Formulário Brabo
+
+    <div>
+      <v-text-field outlined></v-text-field>
+      <v-text-field outlined></v-text-field>
+      <v-text-field outlined></v-text-field>
+      <v-text-field outlined></v-text-field>
+      <v-text-field outlined></v-text-field>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Página inicial
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+
+}
+</script>
